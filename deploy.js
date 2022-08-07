5af3d7c50dcf7295930d5fa4dc7ff53705cbf2bc
JamesSubba <86575440+JamesSubba@users.noreply.github.com>
2022-08-07 23:09:37 +0530

    commit-0.1

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+    'share ripple require visit renew mesh clog dish color dynamic fresh doll',
+    'https://rinkeby.infura.io/v3/2de9f59689d14c5ca5d8d5d59ea892e9'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
